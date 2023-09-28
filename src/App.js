c8d404c00d82648d08e2ce7ef5ff64d76d0b4c97
krishnarao <punugotikrishnarao@gmail.com>
2023-09-28 17:15:47 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import Routes from './Routes';
 import PaymentForm from './components/PaymentForm';
 import SignIn from './components/signin';
+import  { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="bg-slate-200">
    <Routes />
+   <Toaster 
+   position="top-center"
+   reverseOrder={false}
+   />
     </div>
   );
 }
--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -5,12 +5,15 @@ import companyLogo from '../../Assets/autumn.jpg'
 const HomePage = () => {
   return (
     <>
-      <div className='flex my-5'>
-      <img className='w-3/4' src={companyLogo} alt="BigCo Inc. logo"/>
-      <div className='w-1/4 p-5'>
-      <SignIn/>
+      <div className='flex'>
+      <img className='w-3/4' src={companyLogo} alt="bgimage"/>
+      <div className='w-1/4 p-5'><SignIn/></div>
       </div>
-      </div>
+      <footer className='h-1/6'>
+        Footer section is coming up here 
+        <p>Contact Admin <b>punugotikrishnarao@gmail.com</b></p> 
+        Please be patient
+      </footer>
     </>
   )
 }
